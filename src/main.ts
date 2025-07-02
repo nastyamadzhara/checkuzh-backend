6e61ae4c7880b46472cdfe3e677751e4b58cfe45
daydreamrrrr <nastyamadzara@gmail.com>
2025-07-02 18:04:27 +0300

    feat: enable CORS for frontend integration

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+  await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
